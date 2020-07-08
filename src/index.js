0cd38463f4bd4bace937f17424964321a7763cad
karthi07 <itkarthi02@gmail.com>
2020-07-08 15:08:03 +0530

    fix btn

--- src/index.js
+++ src/index.js
@@ -27,11 +27,11 @@ const addTodo = (todo) => {
         <div class="card-header">Header</div>
         <div class="card-body text-primary">
           <h5 class="card-title">${todo.title}</h5>
-          <p class="card-text">${todo.desc}</p>
+          <p class="card-text">${todo.description}</p>
           <span class="badge badge-pill badge-warning">${todo.priority}</span>
         </div>
         <div class="card-footer">
-          <small class="text-muted">${todo.date}</small>
+          <small class="text-muted">${todo.dueDate}</small>
         </div>
       </div>
     </div>
@@ -39,10 +39,10 @@ const addTodo = (todo) => {
   todoCtn.innerHTML += cardHtml;
 };
 
-const form = document.querySelector('#form');
+const btn = document.querySelector('#submitBtn');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+btn.addEventListener('submit', (e) => {
+  // e.preventDefault();
   const title = document.querySelector('#todo-title').value;
   const desc = document.querySelector('#todo-desc').value;
   const date = document.querySelector('#todo-date').value;
