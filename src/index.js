dc6e3ab5e6274beed159214821c2f1456e73e015
evansinho <igiri.evanson@gmail.com>
2020-07-08 10:24:43 +0100

    add logic to create a todo

--- src/index.js
+++ src/index.js
@@ -18,8 +18,36 @@ export class Todo {
   }
 }
 
-// const todoBtn = document.querySelector('#add-todo');
+const addTodo = (todo) => {
+  const todoCtn = document.querySelector('#todo-list');
 
-// todoBtn.addEventListener('click', (e) => {
-//   e.preventDefault();
-// });
+  const cardHtml = `
+    <div class="col mb-4">
+      <div class="card border-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header">Header</div>
+        <div class="card-body text-primary">
+          <h5 class="card-title">${todo.title}</h5>
+          <p class="card-text">${todo.desc}</p>
+          <span class="badge badge-pill badge-warning">${todo.priority}</span>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">${todo.date}</small>
+        </div>
+      </div>
+    </div>
+  `;
+  todoCtn.innerHTML += cardHtml;
+};
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#todo-title').value;
+  const desc = document.querySelector('#todo-desc').value;
+  const date = document.querySelector('#todo-date').value;
+  const priority = document.querySelector('#todo-priority').value;
+  // instantiate the todo class
+  const todo = new Todo(title, desc, date, priority);
+  addTodo(todo);
+});
